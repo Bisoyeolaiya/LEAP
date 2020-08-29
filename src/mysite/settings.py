9299170f25f04e59afadfe9adace084d62e65a49
Bisoyeolaiya <olaiyabisoye@gmail.com>
2020-08-29 11:51:53 +0100

    Settings file updated

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vksi61fhsvdka$w^zv41%*woz41vz92c9ysv82p9vvq+(#^59%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
